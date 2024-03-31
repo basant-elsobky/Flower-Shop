693f83dee4ae0583ceb7d9b847f2f69b43c93a3f
Ahmed Saber <ahmed0saber33@gmail.com>
2024-04-01 00:55:56 +0200

    Add email as comment

--- src/app/page.js
+++ src/app/page.js
@@ -21,24 +21,40 @@ import Five from "./COMPONENTS/FIVESECTION/Five";
 export default async function Home() {
   // useEffect(() => {
   //   AOS.init({});
-  
+
   // }, [])
+  // const name = "Ahmed Saber"
+  // const email = "ahmed0saber33@gmail"
+  // const message = "Hello Saber!"
 
- 
 
- 
+  // fetch("https://sendmail-api-docs.vercel.app/api/send", {
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //     to: "ahmed0saber33@gmail.com",
+  //     subject: "Flower Shop | Contact US",
+  //     message: `
+  //         <p style>Name: ${name}</p>
+  //         <p>Email: ${email}</p>
+  //         <p>Message: ${message}</p>
+  //       `,
+  //   })
+  // })
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
 
 
-  
+
+
   return (
-   
-    <main >
-   
- <App/>
- <Second />
 
- <Four/>
- <Five/>
+    <main >
+
+      <App />
+      <Second />
+
+      <Four />
+      <Five />
     </main>
   );
 }
