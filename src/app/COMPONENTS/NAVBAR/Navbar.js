15e5229ddd976731c0adb84438f47b7e6e259310
basant-elsobky <124135940+basant-elsobky@users.noreply.github.com>
2024-04-02 06:28:52 +0200

    Resolve conflicts

--- src/app/COMPONENTS/NAVBAR/Navbar.js
+++ src/app/COMPONENTS/NAVBAR/Navbar.js
@@ -23,7 +23,7 @@ function Navbar() {
     const [isMenuOpen, setMenuOpen] = useState(false);
     const [scrolling, setScrolling] = useState(false);
 
-    
+
     useEffect(() => {
         setUser(getUserFromLocalStorage())
 
@@ -31,10 +31,10 @@ function Navbar() {
             const { error, data } = await supabase
                 .from('CartItems')
                 .select()
-                .eq('user-id',user.id)
+                .eq('user-id', user.id)
 
             setcartcountt(data.length)
-       
+
         }
         getData();
     }, []);
@@ -101,9 +101,8 @@ function Navbar() {
                         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
                     </svg>
                     <div className="dropdown-content">
-                        {user?.email ? (
-                            <li>Hello
-                                {user?.email}</li>
+                        {user && user.email ? (
+                            <li>Hello {user.email}</li>
                         ) : ''}
                         <Link href='/Account/Login'>
                             <li>Login</li>
